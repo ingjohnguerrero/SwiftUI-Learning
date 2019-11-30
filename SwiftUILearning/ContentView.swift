6cbb13b36eab8e29897ae8f88d6276c22080bc58
John Guerrero <john.guerrero@peiky.com>
2019-11-29 19:40:28 -0500

    Initial Commit

---
+++ SwiftUILearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILearning
+//
+//  Created by John Edwin Guerrero Ayala on 29/11/19.
+//  Copyright © 2019 John Edwin Guerrero Ayala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
