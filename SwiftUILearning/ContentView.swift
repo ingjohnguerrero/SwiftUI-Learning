53b46035f7927e23e7d647d33fb45c3aaf897fcb
John Guerrero <ingjohnguerrero@gmail.com>
2019-12-01 23:16:35 -0500

    Feat (ListView): Add ListView prototype
    * Add cell prototype for list

--- SwiftUILearning/ContentView.swift
+++ SwiftUILearning/ContentView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        CardView()
+        VStack {
+            CardImageView()
+            List {
+                CellView()
+                CellView()
+                CellView()
+                CellView()
+            }
+        }
     }
 }
 
